72092b8207d03cf74aeecd817bca55f0db52350a
woodbine <ian@spendnetwork.com>
2015-04-20 19:34:29 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,23 +1,56 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import urllib2
+from datetime import datetime
+from bs4 import BeautifulSoup
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on morph.io for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+# Set up variables
+entity_id = "E5021_WLBC_gov"
+url = "http://www.wandsworth.gov.uk/downloads/200110/council_budgets_and_spending"
+
+# Set up functions
+def convert_mth_strings ( mth_string ):
+	month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
+	#loop through the months in our dictionary
+	for k, v in month_numbers.items():
+		#then replace the word with the number
+		mth_string = mth_string.replace(k, v)
+	return mth_string
+
+# pull down the content from the webpage
+html = urllib2.urlopen(url)
+soup = BeautifulSoup(html)
+
+# find all entries with the required class
+block = soup.find('section', {'class':'page-content inner_content'})
+pageLists = block.findAll('li')
+
+for pageList in pageLists:
+  pageUrl = pageList.a['href']
+  
+  if 'CSV' in pageList.text: # Looks for 'CSV' in the text of the list tag
+  	html2 = urllib2.urlopen(pageUrl)
+  	soup2 = BeautifulSoup(html2)
+  	
+  	fileLinks = soup2.findAll('a', href=True)
+  	
+  	for fileLink in fileLinks:
+  		url = fileLink['href']
+  		parsed_link = urlparse.urlsplit(url.encode('utf8'))
+  		parsed_link = parsed_link._replace(path=urllib.quote(parsed_link.path))
+  		encoded_link = parsed_link.geturl()
+  		if encoded_link.startswith('/Council_government_'):
+  			encoded_link = 'http://www.luton.gov.uk'+encoded_link
+  			
+  		if '.xls' in url:
+  			# create the right strings for the new filename
+  			title = fileLink.text
+  			csvYr = title.split(' ')[1]
+  			csvMth = title.split(' ')[0][:3]
+  			csvMth = csvMth.upper()
+  			csvMth = convert_mth_strings(csvMth);
+  			filename = entity_id + "_" + csvYr + "_" + csvMth + ".csv"
+  			todays_date = str(datetime.now())
+  			scraperwiki.sqlite.save(unique_keys=['l'], data={"l": encoded_link, "f": filename, "d": todays_date })
+  			print filename
